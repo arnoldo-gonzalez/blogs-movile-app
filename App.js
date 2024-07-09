4c80c1b404206afc90d124168541b45a6d2bc2db
Arnoldo Gonzalez <arnoldogonzalezroman@gmail.com>
2024-07-09 04:56:37 -0400

    Cambio de libreria de enrutado

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
-import { ScrollView, Dimensions } from 'react-native';
-import { NativeRouter, Routes, Route, Navigate } from "react-router-native";
-import AppBar from "./src/components/AppBar.jsx";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { Text } from "react-native";
+
 import Home from "./src/pages/Home.jsx";
-import Posts from "./src/pages/Posts.jsx";
 import About from "./src/pages/About.jsx";
+import Posts from "./src/pages/Posts.jsx";
+import { theme } from "./src/theme.js";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NativeRouter>
-        <ScrollView>
-            <AppBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/posts" element={<Posts />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </ScrollView>
-    </NativeRouter>
+    <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home" screenOptions={{
+            headerStyle: {
+                backgroundColor: theme.colors.primary
+            },
+            drawerContentStyle: {
+                backgroundColor: theme.colors.primary
+            },
+            drawerActiveTintColor: "#fff",
+            drawerActiveBackgroundColor: theme.colors.secondary,
+            drawerInactiveTintColor: "#fff",
+            drawerIcon: (...props) => (<Text>{console.log(props), "Hola"}</Text>)
+        }}>
+            <Drawer.Screen name="Home" component={Home} options={{ drawerLabel: 'Home' }} />
+            <Drawer.Screen name="Post" component={Posts} options={{ drawerLabel: 'Posts' }} />
+            <Drawer.Screen name="About" component={About} options={{ drawerLabel: 'About' }} />
+        </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
